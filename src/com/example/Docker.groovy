7f11d7266efc1bee83bc8aa3d020d85de506598c
bondgh0954 <otengdevops@gmail.com>
2024-06-11 23:00:19 +0200

    modify

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -18,7 +18,7 @@ class Docker implements Serializable{
     dockerLogin(){
         script.echo 'logging in to dockerhub repository'
         script.withCredentials([script.usernamePassword(credentialsId:'dockerhub-credentials',usernameVariable:'USER',passwordVariable:'PASS')]){
-            script.sh "echo '${PASS}' | docker login -u '${USER}' --password-stdin"
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
         }
 
     }
